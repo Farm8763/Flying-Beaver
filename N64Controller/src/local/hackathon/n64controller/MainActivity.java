410368636b4eb21189be4bc214c97813c7d197b6
fedor_000 <fedor_000@Gavin-laptop>
2013-09-28 21:20:39 -0400

    Important Message about Jared

--- N64Controller/src/local/hackathon/n64controller/MainActivity.java
+++ N64Controller/src/local/hackathon/n64controller/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    //Jared sucks.
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
