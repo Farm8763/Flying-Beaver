8415d29d9da6e86657ec2d823e82cb1c35356653
fedor_000 <fedor_000@Gavin-laptop>
2013-09-28 23:07:13 -0400

    Added UdpConnection class

--- N64Controller/src/local/hackathon/n64controller/MainActivity.java
+++ N64Controller/src/local/hackathon/n64controller/MainActivity.java
@@ -1,8 +1,13 @@
 package local.hackathon.n64controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.graphics.*;
 //blah
 public class MainActivity extends Activity {
@@ -13,6 +18,15 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    public void UDPtest(View button) throws UnknownHostException {
+    	Log.i("MyActivity", "Jared is a nice person...");
+    	
+    	InetAddress ip_address = InetAddress.getByName("192.168.1.17");
+    	int socket = 6969;
+    	Thread t = new Thread(new UdpConnection(ip_address, socket));
+    	t.start();
+    }
+    
     //Jared sucks.
 
     @Override
@@ -21,5 +35,6 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+   
     
 }
