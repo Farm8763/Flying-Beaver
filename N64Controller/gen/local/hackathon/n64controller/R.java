b3b153c16b505211da1345158c934c45f8737325
fedor_000 <fedor_000@Gavin-laptop>
2013-09-28 23:09:06 -0400

    da fuq

--- N64Controller/gen/local/hackathon/n64controller/R.java
+++ N64Controller/gen/local/hackathon/n64controller/R.java
@@ -30,10 +30,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0003;
-        public static final int button1=0x7f0a0001;
-        public static final int button2=0x7f0a0002;
-        public static final int textView1=0x7f0a0000;
+        public static final int action_settings=0x7f0a0002;
+        public static final int testUDP=0x7f0a0001;
+        public static final int zoomButton1=0x7f0a0000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
