1f13253012227b58ca0190e1984f80d993bb2f3c
Jared Windover <jaredwindover@gmail.com>
2013-09-28 22:18:35 -0400

    Adding Configure Activity

---
+++ N64Controller/src/local/hackathon/n64controller/ConfigureActivity.java
@@ -0,0 +1,22 @@
+package local.hackathon.n64controller;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ConfigureActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_configure);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.configure, menu);
+		return true;
+	}
+
+}
